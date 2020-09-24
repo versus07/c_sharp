83905054eb6fc68560a650f502f55666f5f362c7
Alesja Sobol <alesja.sobol.yandex.ru>
2020-09-24 21:54:26 +0300

    исправила задачу 4

--- 27_09_2020/task_4/task_4/Program.cs
+++ 27_09_2020/task_4/task_4/Program.cs
@@ -3,41 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace task_4
+
+/*
+ * Даны целые положительные числа A, B, C. Значение этих чисел программа должна запрашивать у пользователя.
+ * На прямоугольнике размера A * B размещено максимально возможное количество квадратов со стороной C.
+ * Квадраты не накладываются друг на друга. Найти количество квадратов, размещенных на прямоугольнике,
+ * а также площадь незанятой части прямоугольника.
+ * Необходимо предусмотреть служебные сообщения в случае, если в прямоугольнике нельзя разместить ни
+ * одного квадрата со стороной С
+ */
+namespace _4
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите сторону прямоугольника а");
-            double side_pramoygol_a = Convert.ToDouble(Console.ReadLine());
+            int A, B, C;
+            Console.WriteLine("Введите размеры прямоугольника A и B ");
+            A = Convert.ToInt32(Console.ReadLine());
+            B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите размеры квадрата C ");
+            C = Convert.ToInt32(Console.ReadLine());
+            int s = A * B;
+            int kolvo_kv = s / Convert.ToInt32(Math.Pow(C, 2));
+            int s_nazanyato = s % Convert.ToInt32(Math.Pow(C, 2));
+            Console.WriteLine($"В прямоугольнике размером {A}x{B} можно разместить {kolvo_kv} квадратов со стороной {C}");
+            Console.WriteLine($"Площадь незанятая - {s_nazanyato}");
 
-            Console.WriteLine("Введите сторону прямоугольника b");
-            double side_pramoygol_b = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Введите сторону квадрата");
-            double side_square = Convert.ToDouble(Console.ReadLine());
-
-            
-            if (side_pramoygol_a < side_square || side_pramoygol_b < side_square)
-            {
-                Console.WriteLine("Одна из сторон квадрата или обе больше прямоугольника");
-
-            }
-            else
-            {
-                double col_squares_side_a = side_pramoygol_a / side_square;
-                double col_squares_side_b = side_pramoygol_b / side_square;
-                double col_squares = col_squares_side_a * col_squares_side_b;
-                double S_pramougol = side_pramoygol_a * side_pramoygol_b;
-                double S_square = 4 * side_square;
-                double S_square_all = S_square * col_squares;
-                double ostatok = S_pramougol - S_square_all;
-
-                Console.WriteLine("Всего квадратов " + col_squares);
-                Console.WriteLine("Остаток " + ostatok);
-
-            }
         }
     }
 }
