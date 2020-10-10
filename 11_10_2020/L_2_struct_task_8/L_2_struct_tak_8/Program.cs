f7900a5c37d8fd651ab9b5cd0afd35a55bad423d
Alesja Sobol <alesja.sobol.yandex.ru>
2020-10-10 19:29:43 +0300

    new task and I stuck in main fanction and in get

---
+++ 11_10_2020/L_2_struct_task_8/L_2_struct_tak_8/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace L_2_struct_task_8
+{
+
+    struct Article
+    {     
+        public int product_code;
+        public string product_name;
+        public double product_price;
+              
+
+        enum ArticleTips
+        {
+            refrigerator,
+            microwave_oven,
+            iron,
+            toaster,
+            washing_machine,
+            vacuum_cleaner,
+            TV_set
+        }      
+    }
+
+    struct Client
+    {
+        private string last_name_first_name_patronymic;
+        private int address;
+        private int telephone;
+        private int col_of_orders;
+        private int all_sum_orders;
+
+        enum ClientType
+        {
+            client_payment_up_to_100,
+            client_payment_up_to_200,
+            client_payment_up_to_500,
+            client_payment_up_to_1000,
+            client_VIP_more_1000
+        }
+    }
+
+    struct RequestItem
+    {
+        private string product;
+        private int quantity_product;       
+    }
+
+    struct Request
+    {
+        private int order_code;
+        private string client;
+        private int order_date;
+        private int list_ordered_products;
+
+        public int order_sum////////
+        {
+        get
+            {
+               return this.//cant see enather struct;
+            }
+        }
+    
+        enum PayType
+        {
+            clearing_settlement,
+            cash
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Article ar = new Article(20, ";lk", 2.5);
+
+            ar.GetType();
+          
+
+             //ar.ArticleTips = new Article();
+             //ar = new Article();
+             //Console.WriteLine(ar.ArticleTips + " ");
+
+        }
+    }
+}
